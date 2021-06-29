85d505f713be1671439b405bad8f156d22f2c367
Dykann Khaov <dykann.khaov@gmail.com>
2021-06-29 18:08:33 +0200

    improve images quality

--- pages/diagnostic.js
+++ pages/diagnostic.js
@@ -1,22 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 import Section from '/components/section'
 import Image from 'next/image'
+import infoDyson from "../public/images/details.PNG"
+import competitorsOfDyson from "../public/images/concurrence.PNG"
 
 function Diagnostic() {
   return (
     <Section className="lg:flex lg:justify-evenly lg:h-screen h-full md:py-6 py-0">
       <div className="flex lg:flex-row flex-col items-center ">
-        <Image src="/images/details.PNG"
-          alt="diagnostic dyson"
-          width="550"
-          height="600"
+        <Image src={infoDyson}
+          alt="Diagnostic Dyson"
           quality="100"
           priority
         />
-        <Image src="/images/concurrence.PNG"
-          alt="concurrence dyson"
-          width="620"
-          height="400"
+        <Image src={competitorsOfDyson}
+          alt="Concurrence Dyson"
           quality="100"
           priority
         />
--- pages/econtenu.js
+++ pages/econtenu.js
@@ -1,15 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 import Section from '/components/section'
 import Image from 'next/image'
+import eContenu from "../public/images/econtenu.PNG"
+
 
 function Econtenu() {
   return (
     <Section className="h-screen lg:px-6 px-0 lg:justify-start flex items-center justify-center">
       <div>
-        <Image src="/images/econtenu.PNG"
-          alt="econtenu"
-          width="600"
-          height="550"
+        <Image src={eContenu}
+          alt="E-contenu"
           quality="100"
           priority
         />
