929eb8aeb652cd322a2afba278efb27895c7c006
Dykann Khaov <dykann.khaov@gmail.com>
2021-12-20 01:14:40 +0100

    reference website and add dyson favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css'
 import '../styles/global.css'
+import Head from 'next/head'
 import Header from "/components/header"
 import Footer from "/components/footer"
 
@@ -8,6 +9,11 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <title>Dykann Khaov Dyson</title>
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
